7b8415581d802e938f2c800b26840b07a7279b80
harkion <gencfahrican@outlook.com>
2026-03-24 10:21:31 +0100

    Initial Commit

---
+++ Zest ZooTests/Zest_ZooTests.swift
@@ -0,0 +1,17 @@
+//
+//  Zest_ZooTests.swift
+//  Zest ZooTests
+//
+//  Created by Fahri Can on 24/03/2026.
+//
+
+import Testing
+@testable import Zest_Zoo
+
+struct Zest_ZooTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
